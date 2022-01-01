b2f5fa52a7049b0054c7fa284dc3cd4a77a12331
vahvake <62074587+vahvake@users.noreply.github.com>
2022-01-01 16:08:45 +0200

    Add routes or something

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,1 +1,32 @@
-console.log("js IS loaded");
+const router = async () => {
+  const routes = [
+    { path: "/", view: () => console.log("Viewing Dashboard") },
+    { path: "/work", view: () => console.log("Viewing Work") },
+    { path: "/bio", view: () => console.log("Viewing Bio") },
+    { path: "/history", view: () => console.log("Viewing History") }
+  ];
+
+  // Test each route for potential match
+  const potentialMatches = routes.map( route => {
+    return {
+      route: route,
+      isMatch: location.pathname === route.path
+    };
+  });
+
+  let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+
+  //Default to dashboard if no match
+  if (!match) {
+    match = {
+      route: routes[0],
+      isMatch: true
+    };
+  };
+
+  match.route.view();
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  router();
+});
