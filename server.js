3e29c15af3bd7238ff7cfc24eea790908e2541db
vahvake <62074587+vahvake@users.noreply.github.com>
2022-01-11 17:53:42 +0200

    Add robots.txt and sitemap.xml

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const app = express();
 //Static folder works for stuff
 app.use('/static', express.static(path.resolve(__dirname, 'frontend', 'static')));
 
+app.get('/robots.txt', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'robots.txt'));
+});
+app.get('/sitemap.xml', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'sitemap.xml'));
+});
+
 //Every path to Index.html
 app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'frontend', 'index.html'));
